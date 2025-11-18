ee5ab95ca07be4d42fe43af92df0431e185f7e70
Lucas Espitia <espitia.174114@e.unavarra.es>
2025-11-18 15:29:46 +0100

    Creating basic Skeleton in Java

---
+++ CareHomeApp/src/main/java/com/msoft/carehomeapp/CareHomeApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.msoft.carehomeapp;
+
+/**
+ *
+ * @author lucas
+ */
+public class CareHomeApp  {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
